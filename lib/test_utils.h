7162aa7d1f60e81238b346c78504e730d68f96e5
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-21 22:24:59 +1000

    Update test utils

--- lib/test_utils.h
+++ lib/test_utils.h
@@ -6,12 +6,36 @@
 
 using namespace std;
 
+enum Operation
+{
+    lessThan,
+    greaterThan,
+};
+
 // Basic testing helper:
 template <typename T, typename K>
 void expect(string name, T lhs, K rhs)
 {
     lhs == rhs ? cout << "\033[32m" << name << ":\tpassed!\n" : cout << "\033[31m" << name << ":\tfailed...\n\texpected: " << rhs << "\n\treceived: " << lhs << "\n";
     cout << "\033[0m";
+}
+
+template <typename T, typename K>
+void expect(string name, T lhs, Operation operation, K rhs)
+{
+    switch (operation)
+    {
+    case lessThan:
+        lhs < rhs ? cout << "\033[32m" << name << ":\tpassed!\n" : cout << "\033[31m" << name << ":\tfailed...\n\texpected: " << rhs << "\n\treceived: " << lhs << "\n";
+        cout << "\033[0m";
+        break;
+    case greaterThan:
+        lhs > rhs ? cout << "\033[32m" << name << ":\tpassed!\n" : cout << "\033[31m" << name << ":\tfailed...\n\texpected: " << rhs << "\n\treceived: " << lhs << "\n";
+        cout << "\033[0m";
+        break;
+    default:
+        cout << "Invalid operation\n";
+    }
 }
 
 #endif // TEST_UTILS_H
